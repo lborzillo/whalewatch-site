811495bdf22093479a711eaa2b2e9206e00ab9ba
lborzillo <lborzillo@gmail.com>
2025-05-20 08:59:43 -0400

    Add files via upload

--- public/whalewatch.js
+++ public/whalewatch.js
@@ -1,5 +1,7 @@
 
 function toggleWhaleBot() {
   const panel = document.getElementById('whalebotPanel');
-  panel.style.display = panel.style.display === 'block' ? 'none' : 'block';
+  if (panel) {
+    panel.style.display = panel.style.display === 'block' ? 'none' : 'block';
+  }
 }
